de4b771c45db8bd56f6e23a7f4bb0194317fbff7
Pierre-Emile Ferron <peferron@gmail.com>
2015-06-02 22:43:42 -0700

    Add demo

--- demo/index.js
+++ demo/index.js
@@ -3,11 +3,26 @@ $(function() {
         var input = $('#input').val();
         var output = encodeURIComponent(input);
 
-        // pbp.save({
-        //     input: input,
-        //     output: output
-        // });
+        playbyplay.save({
+            input: input,
+            output: output
+        });
 
         $('#output').text(output);
+    });
+
+    $('#load').on('click', function() {
+        var $header = $('<tr><th>Input</th><th>Output</th></tr>');
+
+        var runs = playbyplay.load();
+        var $runs = runs.reverse().map(function(run) {
+            var $input = $('<td></td>').text(run.input);
+            var $output = $('<td></td>').text(run.output);
+            return $('<tr></tr>').append($input, $output);
+        });
+
+        var $history = $('<table></table>').append($header, $runs);
+
+        $('#history').html($history);
     });
 });
