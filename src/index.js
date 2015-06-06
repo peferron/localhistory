527f6d857358d017a05cb74437f0ee28528b3d9b
Pierre-Emile Ferron <peferron@gmail.com>
2015-06-06 02:15:50 -0700

    Prevent callback from being invoked twice if it throws

--- src/index.js
+++ src/index.js
@@ -3,32 +3,45 @@ import * as storage from './storage';
 
 export function save(run, callback) {
     setTimeout(() => {
+        let err = null;
         try {
             support.check();
             storage.save(run);
-            callback(null);
-        } catch (err) {
+        } catch (e) {
+            err = e;
+        }
+
+        if (callback) {
             callback(err);
         }
     }, 0);
 }
 
 export function load(callback) {
+    let err = null;
+    let runs;
     try {
         support.check();
-        const runs = storage.load();
-        callback(null, runs);
-    } catch (err) {
-        callback(err);
+        runs = storage.load();
+    } catch (e) {
+        err = e;
+    }
+
+    if (callback) {
+        callback(err, runs);
     }
 }
 
 export function clear(callback) {
+    let err = null;
     try {
         support.check();
         storage.clear();
-        callback(null);
-    } catch (err) {
+    } catch (e) {
+        err = e;
+    }
+
+    if (callback) {
         callback(err);
     }
 }
