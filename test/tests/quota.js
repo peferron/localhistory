77ee555a7cb7fdbed11fc19b30f1ae59cb9d175d
Pierre-Emile Ferron <peferron@gmail.com>
2015-06-10 00:02:19 -0700

    Add test where localStorage quota triggers a trim

--- test/tests/quota.js
+++ test/tests/quota.js
@@ -1,9 +1,10 @@
-describe('after filling localStorage quota', () => {
-    const key = 'playbyplay_trash_K79kxaC%I8HrRGDoousr';
-
+describe('after clearing', () => {
     let tooLong;
+    let halfTooLong1;
+    let halfTooLong2;
 
-    beforeEach(done => {
+    function init() {
+        const key = 'playbyplay_trash_K79kxaC%I8HrRGDoousr';
         tooLong = 'startups would kill to grow this fast';
         while (true) { // eslint-disable-line no-constant-condition
             tooLong += tooLong;
@@ -13,16 +14,34 @@ describe('after filling localStorage quota', () => {
                 break;
             }
         }
-        done();
+        localStorage.removeItem(key);
+
+        halfTooLong1 = tooLong.substring(0, tooLong.length / 2);
+        halfTooLong2 = halfTooLong1.substring(0, halfTooLong1.length - 1) + '$';
+    }
+
+    beforeEach(async () => {
+        await playbyplay.clear();
+        if (!tooLong) {
+            init();
+        }
     });
 
-    it('should return a quota error on save', () =>
+    it('should return a quota error when saving a run that cannot fit in localStorage', () =>
         expect(playbyplay.save(tooLong, {maxBytes: Infinity})).to.be.rejectedWith(Error,
             `Could not save run of length ${tooLong.length + 4}, exceeds localStorage quota`)
     );
 
-    after(async () => {
-        localStorage.removeItem(key);
-        await playbyplay.clear();
+    describe('and saving two runs that cannot both fit in localStorage', () => {
+        beforeEach(async () => {
+            await playbyplay.save(halfTooLong1, {maxBytes: Infinity});
+            await playbyplay.save(halfTooLong2, {maxBytes: Infinity});
+        });
+
+        it('should load the last run only', () =>
+            expect(playbyplay.load()).to.eventually.deep.equal([halfTooLong2])
+        );
     });
+
+    afterEach(playbyplay.clear);
 });
