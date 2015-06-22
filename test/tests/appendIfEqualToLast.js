ea61102e28a023560bfc42063f2e9cc38f3f23d3
Pierre-Emile Ferron <peferron@gmail.com>
2015-06-21 21:08:42 -0700

    Simplify appendIfEqualToLast test

--- test/tests/appendIfEqualToLast.js
+++ test/tests/appendIfEqualToLast.js
@@ -1,26 +1,25 @@
-describe('after clearing', () => {
-    beforeEach(() => localhistory.clear('test'));
+describe('after clearing and appending a first entry', () => {
+    const first = {first: '1'};
 
-    describe('and appending a first entry', () => {
-        const first = {first: '1'};
+    beforeEach(async () => {
+        await localhistory.clear('test');
+        await localhistory.append('test', first);
+    });
 
+    describe('and appending the first entry again with appendIfEqualToLast not set', () => {
         beforeEach(() => localhistory.append('test', first));
 
-        describe('and appending the first entry again with appendIfEqualToLast not set', () => {
-            beforeEach(() => localhistory.append('test', first));
+        it('should load the first entry twice', () =>
+            expect(localhistory.load('test')).to.eventually.deep.equal([first, first])
+        );
+    });
 
-            it('should load the first entry twice', () =>
-                expect(localhistory.load('test')).to.eventually.deep.equal([first, first])
-            );
-        });
+    describe('and appending the first entry again with appendIfEqualToLast = false', () => {
+        beforeEach(() => localhistory.append('test', first, {appendIfEqualToLast: false}));
 
-        describe('and appending the first entry again with appendIfEqualToLast = false', () => {
-            beforeEach(() => localhistory.append('test', first, {appendIfEqualToLast: false}));
-
-            it('should load the first entry only once', () =>
-                expect(localhistory.load('test')).to.eventually.deep.equal([first])
-            );
-        });
+        it('should load the first entry only once', () =>
+            expect(localhistory.load('test')).to.eventually.deep.equal([first])
+        );
     });
 
     after(() => localhistory.clear('test'));
